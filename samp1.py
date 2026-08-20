82ac38bf8a99e53252971fadfe4b96a1d651a5c8
ANYU-PYTHON <pythonlove11@outlook.com>
2026-08-21 00:02:42 +0900

    delete

--- samp1.py
+++
@@ -1,2 +0,0 @@
-import streamlit as stm
-print(f"text:\n{stm.text_area("text").split("\n")}")
--- samp2.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-import hashlib
-conn = sqlite3.connect("WPMG.db")
-cur = conn.cursor()
-for row in cur.execute("""SELECT * FROM News;"""):
-    print(row)
